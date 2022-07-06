c26178c064a62c17ec540cfa54469714d9a76778
Christopher Debove <chibiblasphem@gmail.com>
2022-07-06 13:47:54 +0200

    feat: add better loading of images

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react';
-import { CardRoot } from './Card.styles';
+import { ReactNode, useEffect, useState } from 'react';
+import { CardBackground, CardRoot } from './Card.styles';
 
 export interface CardProps {
   children?: ReactNode;
@@ -8,8 +8,28 @@ export interface CardProps {
 }
 
 export function Card({ className, background, children }: CardProps) {
+  const [isLoaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!background) {
+      return;
+    }
+
+    const image = new Image();
+    const onLoad = () => {
+      setLoaded(true);
+    };
+    image.addEventListener('load', onLoad);
+    image.src = background;
+
+    return () => {
+      image.removeEventListener('load', onLoad);
+    };
+  }, [background]);
+
   return (
-    <CardRoot className={className} background={background}>
+    <CardRoot className={className}>
+      {isLoaded && <CardBackground background={background} />}
       {children && <div className="overlay">{children}</div>}
     </CardRoot>
   );
